b118d8484e5107cfb3cc0f17454a9ffcbbda87cb
unknown <prashantkumarsheoganj@gmail.com>
2026-03-09 12:13:50 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// Trigger Vercel Deployment
 export default defineConfig({
   plugins: [react()],
 });
