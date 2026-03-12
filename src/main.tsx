8134362fdc21d057b1f530f5d2aa7d68db4ad622
Musafaizan <iammusa182@gmail.com>
2026-03-12 16:02:23 +0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./page.tsx";
+import App from "./page.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
